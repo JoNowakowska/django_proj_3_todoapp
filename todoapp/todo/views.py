4906410341533f6f19937797876adc4482f9a4dc
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-06-21 23:18:29 +0200

    Create todo form and view added

--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 
 
 ERROR_PWD_DONT_MATCH = "Passwords don't match. Please try again."
 ERROR_USERNAME_TAKEN = "This username has been already taken. Please sign up with another username."
 ERROR_WRONG_CREDENTIALS = "Username and password did not match. Please try again."
+ERROR_WRONG_VALUE = "Something went wrong. Please enter correct values into the form."
 
 
 def home(request):
@@ -43,6 +45,20 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
+
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm})
+    elif request.method == 'POST':
+        try:
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm, 'error': ERROR_WRONG_VALUE})
 
 
 def currenttodos(request):
